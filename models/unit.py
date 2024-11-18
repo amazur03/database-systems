148ebaac6db303bf33fc1c76b09162ff67d94637
natpol567 <148222270+natpol567@users.noreply.github.com>
2024-11-18 19:36:02 +0100

    unit.py

---
+++ models/unit.py
@@ -0,0 +1,22 @@
+from app import db
+
+class Unit(db.Model):
+    """
+    Model for the 'units' table.
+    This table defines measurement units for products.
+    """
+
+    __tablename__ = 'units'
+
+    id = db.Column(db.BigInteger, primary_key=True)  # Unique identifier for each unit
+    name = db.Column(db.String, nullable=False, unique=True)  # Name of the unit
+    percentage_of_the_stock = db.Column(db.Numeric, nullable=True)  # Percentage of stock filled by this unit
+
+
+    products = db.relationship('Product', back_populates='unit')  # Relationship to products
+
+    def __repr__(self):
+        """
+        Provides a string representation of the Unit instance.
+        """
+        return f"<Unit(id={self.id}, name='{self.name}')>"
